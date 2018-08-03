7fc9e7214d48183d907ae733b4486634f945932a
Devin Mounts <devinmounts@gmail.com>
2018-08-03 10:41:26 -0700

    mercury year method passes.

--- spec/planetary-life-spec.js
+++ spec/planetary-life-spec.js
@@ -14,16 +14,26 @@ describe('Human', function() {
     let testNow = new Date();
     expect(reusableHuman.name).toEqual("Smitty");
     expect(reusableHuman.dateBirth).toEqual(testBirth);
-    expect(reusableHuman.dateNow.getDate()).toEqual(testNow.getDate());
-    });
+    expect(reusableHuman.dateNow).toEqual(testNow);
+  });
 
     it('should test ageInSeconds() returns Human age in seconds', function(){
       let testNow = new Date();
       let testBirth = new Date("1990-08-03");
       const human = new Human("Smitty", testBirth);
+      expect(human.ageInSeconds()).toEqual((testNow-testBirth) *.001);
+    });
 
-      expect(reusableHuman.ageInSeconds()).toEqual((testNow-testBirth) * .001);
+    it('should test ageMercury() returns Human age on mercury in years', function(){
+      let testNow = new Date();
+      let testBirth = new Date("1990-08-03");
+      let testSec = (testNow-testBirth) * .001;
+      let testAge = testSec / .24
+      const human = new Human("Smitty", testBirth);
+      human.ageInSeconds();
+      expect(human.ageMercury()).toEqual(testAge);
+    });
 
-      });
+
 
 });
