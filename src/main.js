8a41d829199d6eada8b4e793d8f379131b812a1b
Devin Mounts <devinmounts@gmail.com>
2018-08-03 14:56:51 -0700

    create sumbit function

--- src/main.js
+++ src/main.js
@@ -6,5 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 $(document).ready(function(){
-
+  $("#form").submit(function(){
+      let name = $("#name").val();
+      let birthdate = $("#date").val();
+      let gender = $("#gender").val();
+      const human = new Human(name, gender, birthdate);
+      human.runPlanets();
+  });
+  $()
 });
