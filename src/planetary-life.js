89345e91005000871aa55318991a9fd3cbd66e56
Devin Mounts <devinmounts@gmail.com>
2018-08-03 08:50:23 -0700

    create Human class and constructor.

---
+++ src/planetary-life.js
@@ -0,0 +1,11 @@
+class Human {
+  constructor(name, dateBirth) {
+    this.name = name;
+    this.dateNow = new Date();
+    this.dateBirth = new Date(dateBirth);
+  }
+
+  ageInSeconds(dateNow, dateBirth) {
+    let age
+  }
+}
