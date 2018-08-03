a772e2f29ff06f29b493cc54a9d6ad99ea6016bb
Devin Mounts <devinmounts@gmail.com>
2018-08-03 09:34:13 -0700

    update constructor.

--- src/planetary-life.js
+++ src/planetary-life.js
@@ -1,11 +1,13 @@
-class Human {
+export class Human{
   constructor(name, dateBirth) {
     this.name = name;
     this.dateNow = new Date();
     this.dateBirth = new Date(dateBirth);
+    this.age = 0;
   }
 
-  ageInSeconds(dateNow, dateBirth) {
-    let age
-  }
+  // ageInSeconds(dateNow, dateBirth) {
+  //
+  //   let this.age = (dateNow - dateBirth) / 1000;
+  // }
 }
